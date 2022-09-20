ad267fd5455652e5c950e8b470fff253c630a43b
CHOIJIHO <wlghsp@gmail.com>
2022-09-21 07:53:56 +0900

    회원 가입 인증 메일 확인, 회원 가입 인증 메일 확인 테스트 및 리팩토링

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -2,8 +2,6 @@ package com.studyolle.account;
 
 import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -21,6 +19,7 @@ public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
 
     private final AccountService accountService;
+    private final AccountRepository accountRepository;
 
 
 
@@ -44,6 +43,27 @@ public class AccountController {
 
         accountService.processNewAccount(signUpForm);
         return "redirect:/";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model) {
+        Account account = accountRepository.findByEmail(email);
+        String view = "account/checked-email";
+        if (account == null) {
+            model.addAttribute("error", "wrong email");
+            return view;
+        }
+        if (!account.getEmailCheckToken().equals(token)) {
+            model.addAttribute("error", "wrong token");
+            return view;
+        }
+
+
+        account.completeSignUp();
+
+        model.addAttribute("numberOfUser", accountRepository.count());
+        model.addAttribute("nickname", account.getNickname());
+        return view;
     }
 
 
--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.Valid;
 
@@ -17,7 +18,10 @@ public class AccountService {
     private final JavaMailSender javaMailSender;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional // transaction 범위 안에 있게 하는 어노테이션
     public void processNewAccount(SignUpForm signUpForm) {
+        // @Transactional 이 있어
+        // newAccount 는 detached가 아닌 persist 상태가 된다.
         Account newAccount = saveNewAccount(signUpForm);
         newAccount.generateEmailCheckToken();
         sendSignUpConfirmEmail(newAccount);
